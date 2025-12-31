9c1acff41d634a5de6bce6f3c0c1a1a63a348e82
jherron <jherron@net-dev.net>
2025-12-31 15:05:14 -0500

    switch to ubi9

--- ontampa-devs/links/docker-entrypoint.sh
+++ ontampa-devs/links/docker-entrypoint.sh
@@ -30,21 +30,20 @@ printf '|   LINKSTACK v%s%*s|\n' "${v}" "$vlen" | tr ' ' " "
 # + ---------------- + #
 
 echo '+ ------------------------------------------------------------------ +'
-echo '| Updating Configuration: Apache Base (/etc/apache2/httpd.conf)      |'
-echo '| Updating Configuration: Apache SSL  (/etc/apache2/conf.d/ssl.conf) |'
+echo '| Updating Configuration: Apache Base (/etc/httpd/httpd.conf)      |'
+echo '| Updating Configuration: Apache SSL  (/etc/httpd/conf.d/ssl.conf) |'
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -config /etc/ssl/openssl.cnf \
- -keyout /etc/apache2/conf.d/server.key -out /etc/apache2/conf.d/server.pem &> /dev/null
+#openssl req -x509 -nodes -days 365 -newkey rsa:2048 -config /etc/ssl/openssl.cnf -keyout /etc/httpd/conf.d/${HTTP_SERVER_NAME}/key-${HTTP_SERVER_NAME} -out /etc/httpd/conf.d/server.pem
 
-echo '| Updating Configuration: PHP         (/etc/php83/40-custom.ini)     |'
+echo '| Updating Configuration: PHP         (/etc/php.d/40-custom.ini)     |'
 echo "| Setting PHP Configuration:                                         |"
 echo "| upload_max_filesize = ${UPLOAD_MAX_FILESIZE}                       |"
 echo "| memory_limit = ${PHP_MEMORY_LIMIT}                                 |"
 echo "| date.timezone = ${TZ}                                              |"
 
-echo "upload_max_filesize = ${UPLOAD_MAX_FILESIZE}" >> /etc/php83/conf.d/40-custom.ini
-echo "memory_limit = ${PHP_MEMORY_LIMIT}" >> /etc/php83/conf.d/40-custom.ini
-echo "date.timezone = ${TZ}" >> /etc/php83/conf.d/40-custom.ini
+echo "upload_max_filesize = ${UPLOAD_MAX_FILESIZE}" >> /etc/php.d/40-custom.ini
+echo "memory_limit = ${PHP_MEMORY_LIMIT}" >> /etc/php.d/40-custom.ini
+echo "date.timezone = ${TZ}" >> /etc/php.d/40-custom.ini
 
 rm -f /htdocs/httpd.pid
 
