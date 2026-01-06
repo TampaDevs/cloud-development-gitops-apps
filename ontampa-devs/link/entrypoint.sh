4696e7b846c8d3fadf0ff885b0f7d7139f3f2dd9
jherron <jherron@net-dev.net>
2026-01-06 09:21:59 -0500

    sync current status

---
+++ ontampa-devs/link/entrypoint.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+set -eu
+#- 
+export LOG_LEVEL="${LOG_LEVEL:-info}"
+export TZ="${TZ:-UTC}"
+
+#- 
+export HTTP_SERVER_PORT="${HTTP_SERVER_PORT:-8080}"
+export HTTP_SERVER_NAME="${HTTP_SERVER_NAME:-localhost}"
+
+#- 
+export PHP_MEMORY_LIMIT="${PHP_MEMORY_LIMIT:-512M}"
+export UPLOAD_MAX_FILESIZE="${UPLOAD_MAX_FILESIZE:-8M}"
+	
+v="$(cat /htdocs/version.json | tr -d '\r\n')"
+vlen="$((27-${#v}))"
+
+echo   '+ -------------------------------------------------------------- +'
+printf '|   LINKSTACK v%s%*s|\n' "${v}" "$vlen" | tr ' ' " "
+
+echo   '| Current configuration                                          |'
+echo   '| HTTP Port: ${HTTP_SERVER_PORT}                                 |'
+echo   '| Updating Configuration: PHP         (/etc/php.d/40-custom.ini) |'
+echo   '+ ---------------------------------------------------------------+'
+echo   '| Setting PHP Configuration:                                     |'
+echo   "| upload_max_filesize = ${UPLOAD_MAX_FILESIZE}                   |"
+echo   "| memory_limit = ${PHP_MEMORY_LIMIT}                             |"
+echo   "| date.timezone = ${TZ}                                          |" 
+
+echo "upload_max_filesize = ${UPLOAD_MAX_FILESIZE}" >> /etc/php.d/40-custom.ini
+echo "memory_limit = ${PHP_MEMORY_LIMIT}" >> /etc/php.d/40-custom.ini
+echo "date.timezone = ${TZ}" >> /etc/php.d/40-custom.ini
+
+httpd -DFOREGROUND
